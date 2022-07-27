4356afa7e38fdaf567c4a8d9ad9e5dd4c234f673
Sepehr Gorgini <agent.1378@gmail.com>
2022-07-27 17:50:02 +0430

    add pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 
@@ -6,7 +10,14 @@ function App() {
   return (
     <div className='flex flex-col justify-between h-screen align-center'>
       <Navbar title='GitHub Finder' />
-      <main>Main Content</main>
+      <main className='container px-5 pb-12 mx-auto'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/notfound' element={<NotFound />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+      </main>
       <Footer />
     </div>
   )
