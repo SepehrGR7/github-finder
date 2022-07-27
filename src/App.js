fa7a7e268bf635c21b8cac2f3eeba8297dab5fb0
Sepehr Gorgini <agent.1378@gmail.com>
2022-07-27 17:28:22 +0430

    add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <div className='flex flex-col justify-between h-screen align-center'>
       <Navbar title='GitHub Finder' />
-      <main>Main Contnet</main>
+      <main>Main Content</main>
+      <Footer />
     </div>
   )
 }
